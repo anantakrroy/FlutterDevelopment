b551d8b72b8537dd54b3c39de333471f6b9625d1
anantakrroy <kr.innovate@gmail.com>
2018-11-26 19:33:24 +0530

    Working with Containers and Rows

--- widgets_fun/lib/app_screens/home.dart
+++ widgets_fun/lib/app_screens/home.dart
@@ -7,16 +7,80 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        alignment: Alignment.center,
-        color: Colors.deepPurple,
-        // width: 200.0,
-        // height: 100.0,
-        margin: EdgeInsets.only(right:20.0, left: 20.0, top: 30.0),
-        child: Text(
-          "Flight",
-          textDirection: TextDirection.ltr,
-        ),
-      ),
+          padding: EdgeInsets.all(35.0),
+          alignment: Alignment.center,
+          color: Colors.deepPurple,
+          // width: 200.0,
+          // height: 100.0,
+          // margin:
+          // EdgeInsets.only(right: 20.0, left: 20.0, top: 30.0, bottom: 30.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Flight",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontFamily: 'NotoSansTC',
+                      color: Colors.white,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  )),
+                  Expanded(
+                      child: Text(
+                    "Trains",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontFamily: 'NotoSansTC',
+                      color: Colors.white,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  )),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Flight",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontFamily: 'NotoSansTC',
+                      color: Colors.white,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  )),
+                  Expanded(
+                      child: Text(
+                    "Trains",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontFamily: 'NotoSansTC',
+                      color: Colors.white,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  )),
+                ],
+              ),
+              TravelImageAsset()
+            ],
+          )),
     );
   }
+}
+
+class TravelImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/travel.jpg');
+    Image image = Image(image: assetImage, width: 250.0,height: 250.0,);
+    return Container(child: image,);
+  }
+
 }
