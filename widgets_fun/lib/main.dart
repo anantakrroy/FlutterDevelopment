f9f77dfe0d0a0ef6811f880672248bdc978543ff
anantakrroy <kr.innovate@gmail.com>
2018-12-03 18:21:53 +0530

    Long lists,Floating action & Snackbar

--- widgets_fun/lib/main.dart
+++ widgets_fun/lib/main.dart
@@ -7,40 +7,91 @@ void main() {
     title: "Exploring UI widgets",
     home: Scaffold(
       appBar: AppBar(
-        title:Text("Basic List View Example"),
+        title:Text("Long List View Example", style: TextStyle(
+          fontFamily: 'Monoton',
+          fontSize: 12.0,
+          fontWeight: FontWeight.w400
+        ),),
       ),
       body: getListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("Add button was clicked");
+      },
+        tooltip: "Add item",
+        child: Icon(Icons.add),
+      ),
     ),
     ));
 }
 
+  void showSnackBar(BuildContext context, String item) {
+    var snackBar = SnackBar(
+      content: Text("$item was clicked"),
+      action: SnackBarAction(label: "UNDO",onPressed: () {
+        debugPrint("Dummy UNDO done!");
+      },),  
+     );
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+  
+
+
+// BASIC LIST VIEW EXAMPLE
+// Widget getListView() {
+//   var listView = ListView(
+//     children: <Widget>[
+//       ListTile(
+//         leading: Icon(Icons.landscape),
+//         title: Text("Landscape"),
+//         subtitle: Text("Beautiful landscapes!"),
+//         trailing: Icon(Icons.wb_cloudy),
+//       ),
+
+//       ListTile(
+//         leading: Icon(Icons.phone),
+//         title: Text("Contact"),
+//         onTap: () {
+//           debugPrint("Contact clicked");
+//         },
+//       ),
+
+//       ListTile(
+//         leading: Icon(Icons.computer),
+//         title: Text("Windows"),
+//       ),
+
+//       Text("Welcome to the contacts app!"),
+
+//       Container(color: Colors.black,padding: EdgeInsets.all(15.0),)
+//     ],
+//   );
+
+//   return listView;
+// }
+
+// LONG LIST VIEW EXAMPLE
+
+// prepare the data 
+List<String> getListElements() {
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
+  return items;
+}
+
+//convert to widgets
 Widget getListView() {
-  var listView = ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful landscapes!"),
-        trailing: Icon(Icons.wb_cloudy),
-      ),
 
-      ListTile(
-        leading: Icon(Icons.phone),
-        title: Text("Contact"),
+  var listItems = getListElements();
+  var listView = ListView.builder(
+    itemBuilder: (context, index) {
+      return ListTile(
+        leading: Icon(Icons.arrow_right),
+        title: Text(listItems[index]),
         onTap: () {
-          debugPrint("Contact clicked");
+          showSnackBar(context, listItems[index]);
         },
-      ),
-
-      ListTile(
-        leading: Icon(Icons.computer),
-        title: Text("Windows"),
-      ),
-
-      Text("Welcome to the contacts app!"),
-
-      Container(color: Colors.black,padding: EdgeInsets.all(15.0),)
-    ],
+      );
+    },
   );
 
   return listView;
