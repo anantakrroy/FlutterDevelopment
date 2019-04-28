bcb7a6578ce067699e5756651f5c0690798cff21
anantakrroy <kr.innovate@gmail.com>
2019-04-28 21:53:35 +0530

    Added Udacity Flutter course exercises

---
+++ udacity-course/course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -0,0 +1,74 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// To keep your imports tidy, follow the ordering guidelines at
+// https://www.dartlang.org/guides/language/effective-dart/style#ordering
+import 'package:flutter/material.dart';
+
+/// A custom [Category] widget.
+///
+/// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
+/// a colored [InkWell] animation.
+class Category extends StatelessWidget {
+  /// Creates a [Category].
+  ///
+  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
+  /// the UI, and the icon that represents it (e.g. a ruler).
+  // TODO: You'll need the name, color, and iconLocation from main.dart
+  var icon;
+  var name;
+  var color;
+  double widgetHeight = 100.0;
+  Category(String n, IconData i, Color c) {
+    this.name = n;
+    this.icon = i;
+    this.color = c;
+  }
+
+  /// Builds a custom widget that shows [Category] information.
+  ///
+  /// This information includes the icon, name, and color for the [Category].
+  @override
+  // This `context` parameter describes the location of this widget in the
+  // widget tree. It can be used for obtaining Theme data from the nearest
+  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
+  // See https://docs.flutter.io/flutter/material/Theme-class.html
+  Widget build(BuildContext context) {
+    // TODO: Build the custom widget here, referring to the Specs.
+    return InkWell(
+      splashColor: Colors.green,
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        height: widgetHeight,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1.0,
+            color: Colors.white,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(widgetHeight / 2)),
+        ),
+        child: Row(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[ 
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Icon(
+                icon,
+                size: 60.0,
+              ),
+            ),
+            Text(
+              name,
+              style: TextStyle(fontSize: 24.0),
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        print("I was tapped");
+      },
+    );
+  }
+}
