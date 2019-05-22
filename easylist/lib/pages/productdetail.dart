35fde0c74ff972c1fcfac20fa9b939ec21b104e2
anantakrroy <kr.innovate@gmail.com>
2019-05-22 09:35:57 +0530

    Added Alert dialogs

--- easylist/lib/pages/productdetail.dart
+++ easylist/lib/pages/productdetail.dart
@@ -7,13 +7,41 @@ class ProductDetail extends StatelessWidget {
 
   ProductDetail(this.title, this.imageUrl);
 
+  _showWarningDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Are you sure?"),
+          content: Text("This action cannot be undone"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Discard"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text("Contnue"),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.pop(context, true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(onWillPop: () {
-      print("Back button pressed");
-      Navigator.pop(context,false);
-      return Future.value(false);
-    },
+    return WillPopScope(
+      onWillPop: () {
+        print("Back button pressed");
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
       child: Scaffold(
         appBar: AppBar(
           title: Text(title),
@@ -32,7 +60,9 @@ class ProductDetail extends StatelessWidget {
               ),
               RaisedButton(
                 child: Text('DELETE'),
-                onPressed: () => Navigator.pop(context, true),
+                onPressed: () {
+                  _showWarningDialog(context);
+                },
                 color: Colors.orange,
               )
             ],
