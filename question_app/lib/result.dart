90677ac58d4645ed398beca4fe5c21005796f633
anantakrroy <kr.innovate@gmail.com>
2019-07-17 11:42:14 +0530

    Generated output for different score ranges

--- question_app/lib/result.dart
+++ question_app/lib/result.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-
   final int resultScore;
 
   Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText = '';
+    if (resultScore < 10) {
+      resultText = 'You are bad!';
+    } else if (resultScore < 20) {
+      resultText = 'You are a bit likeable!';
+    } else if (resultScore < 28) {
+      resultText = 'You are great!';
+    } else {
+      resultText = 'You are amazing!';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Text(
-        'Yay! you have scored $resultScore points!',
+        resultPhrase,
         style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
       ),
     );
   }
